1ab16678ff2343ebb842ec77ae2291c37cd966a0
Henrique Barcelos <hbarcelos909@gmail.com>
2020-04-08 19:14:16 -0300

    feat: added route param substitution helper function

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,3 +3,6 @@ export const TRANSLATOR_DASHBOARD = '/translator';
 export const TRANSLATOR_SETTINGS = '/translator/settings';
 export const TRANSLATION_DASHBOARD = '/translation';
 export const TRANSLATION_CREATION = '/translation/new';
+
+export const withParamSubtitution = route => params =>
+  Object.entries(params).reduce((partialRoute, [name, value]) => partialRoute.replace(`:${name}`, value), route);
