0ff2e4803881f68e8d279ae0439c8d9a623675ec
Henrique Barcelos <hbarcelos909@gmail.com>
2020-04-08 18:06:52 -0300

    refactor: added render prop to RemainingTime component

--- src/components/RemainingTime.jsx
+++ src/components/RemainingTime.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
 import t from 'prop-types';
 import dayjs from 'dayjs';
 import useCountdownTimer from '~/hooks/useCountdownTimer';
 
-const _1_DAY_IN_SECONDS = 24 * 60 * 60;
 const _1_SECOND_IN_MILISSECONDS = 1000;
 const _5_MINUTES_IN_MILISSECONDS = 5 * 60 * 1000;
+const _1_DAY_IN_SECONDS = 24 * 60 * 60;
 
-function RemainingTime({ seconds }) {
+function RemainingTime({ initialValueSeconds, render }) {
   const remainingTime = useCountdownTimer({
-    seconds,
+    seconds: initialValueSeconds,
     updateIntervalMs: remainingTimeInSeconds =>
       remainingTimeInSeconds < _1_DAY_IN_SECONDS ? _1_SECOND_IN_MILISSECONDS : _5_MINUTES_IN_MILISSECONDS,
   });
 
-  const remainingTimeNode =
+  const formattedValue =
     remainingTime === 0
       ? '00:00:00'
       : remainingTime < _1_DAY_IN_SECONDS
       ? dayjs().startOf('day').add(remainingTime, 'second').format('HH:mm:ss')
       : dayjs().add(remainingTime, 'second').fromNow();
 
-  return <>{remainingTimeNode}</>;
+  const endingSoon = remainingTime < _1_DAY_IN_SECONDS;
+
+  return render({ value: remainingTime, formattedValue, endingSoon });
 }
 
+const defaultRender = ({ formattedValue }) => formattedValue;
+
 RemainingTime.propTypes = {
-  seconds: t.number.isRequired,
+  initialValueSeconds: t.number.isRequired,
+  render: t.func,
 };
 
 RemainingTime.defaultProps = {
-  seconds: 0,
+  initialValueSeconds: 0,
+  render: defaultRender,
 };
 
 export default RemainingTime;
