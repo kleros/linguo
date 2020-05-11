73cc813e72aef7e9785555c2e586a60501eb3b23
Henrique Barcelos <hbarcelos909@gmail.com>
2020-05-11 20:42:56 -0300

    refactor(eth value): changed unit selection strategy
    More granular control over the display parameters, allowing to set the maximum number of integer digits. It also inverted the lookup order, trying the larger units before the smaller ones.

--- src/components/EthValue.jsx
+++ src/components/EthValue.jsx
@@ -10,7 +10,11 @@ const { fromWei, toWei, toBN } = Web3.utils;
  * of `getBestDisplayUnit`, because it iterates through this array
  * in order looking for the best match to display a given amount.
  */
-const availableUnitInfoList = [
+const availableUnits = [
+  { unit: 'tether', suffix: { short: 'TETH', long: 'TEther' } },
+  { unit: 'gether', suffix: { short: 'GETH', long: 'GEther' } },
+  { unit: 'mether', suffix: { short: 'METH', long: 'MEther' } },
+  { unit: 'kether', suffix: { short: 'kETH', long: 'kEther' } },
   { unit: 'ether', suffix: { short: 'ETH', long: 'Ether' } },
   { unit: 'milli', suffix: { short: 'mETH', long: 'mEther' } },
   { unit: 'micro', suffix: { short: 'μETH', long: 'μEther' } },
@@ -20,31 +24,35 @@ const availableUnitInfoList = [
   { unit: 'wei', suffix: { short: 'Wei', long: 'Wei' } },
 ];
 
-const indexedAvailableUnitInfo = availableUnitInfoList.reduce(
-  (acc, info) => Object.assign(acc, { [info.unit]: info }),
-  {}
-);
+const indexedAvailableUnit = availableUnits.reduce((acc, info) => Object.assign(acc, { [info.unit]: info }), {});
 
 /**
  * Check if it's possible to display a given `amount` of Wei
- * transforming it to `unit` with up to `decimals` fraction digits.
+ * transforming it to `unit` with up to `decimals` fraction maxIntDigits.
  *
  * @param {object} params The params
  * @param {string|BN} params.amount The amount of Wei
  * @param {'ether'|'milli'|'micro'|'gwei'|'mwei'|'kwei'|'wei'} params.unit The unit for display
- * @param {number} decimals The number of fractional digits.
+ * @param {number} decimals The number of fractional maxIntDigits.
  */
-const canDisplay = ({ amount, unit, decimals }) => {
-  const denominator = toBN(toWei('1', unit));
-  const numerator = toBN(amount).mul(toBN(String(Math.pow(10, decimals))));
+const canDisplay = ({ amount, unit, maxIntDigits, decimals }) => {
+  const intDen = toBN(toWei('1', unit)).mul(toBN(String(Math.pow(10, maxIntDigits))));
+  const intNum = toBN(amount);
 
-  return numerator.div(denominator).gt(toBN('0'));
+  const isIntPartOk = intNum.div(intDen).lt(toBN('1'));
+
+  const frDen = toBN(toWei('1', unit));
+  const frNum = toBN(amount).mul(toBN(String(Math.pow(10, decimals))));
+
+  const isFrPartOk = frNum.div(frDen).gt(toBN('0'));
+
+  return isIntPartOk && isFrPartOk;
 };
 
-const getBestDisplayUnit = ({ amount, decimals }) => {
-  const { bestFit } = availableUnitInfoList.reduce(
+export const getBestDisplayUnit = ({ amount, maxIntDigits = 3, decimals = 2 }) => {
+  const { bestFit } = availableUnits.reduce(
     ({ found, bestFit }, alternative) => {
-      if (found || !canDisplay({ amount, decimals, unit: alternative.unit })) {
+      if (found || !canDisplay({ amount, maxIntDigits, decimals, unit: alternative.unit })) {
         return { found, bestFit };
       }
 
@@ -53,13 +61,23 @@ const getBestDisplayUnit = ({ amount, decimals }) => {
     { found: false }
   );
 
-  return bestFit || indexedAvailableUnitInfo.ether;
+  return bestFit || indexedAvailableUnit.ether;
 };
 
-function EthValue({ amount, decimals, unit, suffixType, render }) {
-  const unitInfo = indexedAvailableUnitInfo[unit] || getBestDisplayUnit({ amount, decimals });
+export const parse = ({ amount, unit = 'ether' }) => {
+  amount = String(unit === 'wei' && typeof amount === 'number' ? Math.trunc(amount) : amount);
+  return toWei(amount, unit);
+};
 
-  const value = Number(fromWei(amount, unitInfo.unit));
+export const valueOf = ({ amount, unit = 'ether' }) => {
+  amount = String(typeof amount === 'number' ? Math.trunc(amount) : amount);
+  return Number(fromWei(amount, unit));
+};
+
+function EthValue({ amount, maxIntDigits, decimals, unit, suffixType, render }) {
+  const unitInfo = indexedAvailableUnit[unit] || getBestDisplayUnit({ amount, maxIntDigits, decimals });
+
+  const value = valueOf({ amount, unit: unitInfo.unit });
 
   return (
     <FormattedNumber
@@ -76,13 +94,15 @@ const defaultRender = ({ formattedValue, suffix }) => `${formattedValue} ${suffi
 
 EthValue.propTypes = {
   amount: t.oneOfType([t.string, t.number]).isRequired,
+  maxIntDigits: t.number,
   decimals: t.number,
-  unit: t.oneOf(['auto', ...Object.keys(indexedAvailableUnitInfo)]),
+  unit: t.oneOf(['auto', ...Object.keys(indexedAvailableUnit)]),
   suffixType: t.oneOf(['none', 'short', 'long']),
   render: t.func,
 };
 
 EthValue.defaultProps = {
+  maxIntDigits: 3,
   decimals: 2,
   suffixType: 'none',
   render: defaultRender,
