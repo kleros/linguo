d43aec6169b02f7a04728b883109dd842e0dc871
Henrique Barcelos <hbarcelos909@gmail.com>
2020-05-18 16:00:34 -0300

    style: fixed InputNumber :hover and :focus styles
    The default styles from antd don't style disabled and with error properly

--- src/adapters/antd/InputNumberWithAddons.jsx
+++ src/adapters/antd/InputNumberWithAddons.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import t from 'prop-types';
 import clsx from 'clsx';
+import styled from 'styled-components';
 import { InputNumber } from 'antd';
 
 function InputNumberWithAddons({ addonBefore, addonAfter, className, ...props }) {
@@ -8,7 +9,7 @@ function InputNumberWithAddons({ addonBefore, addonAfter, className, ...props })
     <div className="ant-input-group-wrapper">
       <div className="ant-input-wrapper ant-input-group">
         {addonBefore && <span className="ant-input-group-addon">{addonBefore}</span>}
-        <InputNumber className={clsx('ant-input', className)} {...props} />
+        <StyledInputNumber className={clsx('ant-input', className)} {...props} />
         {addonAfter && <span className="ant-input-group-addon">{addonAfter}</span>}
       </div>
     </div>
@@ -28,3 +29,18 @@ InputNumberWithAddons.defaultProps = {
 };
 
 export default InputNumberWithAddons;
+
+const StyledInputNumber = styled(InputNumber)`
+  &.ant-input-number-disabled {
+    &,
+    :hover,
+    :focus {
+      background-color: #f5f5f5;
+    }
+
+    :hover,
+    :focus {
+      border-color: #d9d9d9;
+    }
+  }
+`;
