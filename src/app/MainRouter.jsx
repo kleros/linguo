c4d61ffa89ec4806e0e6283c7a3e6a7e907ccb5c
Henrique Barcelos <hbarcelos909@gmail.com>
2020-06-11 10:25:02 -0300

    refactor: extracted Web3ErrorAlert into component

--- src/app/MainRouter.jsx
+++ src/app/MainRouter.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { ConnectedRouter } from 'connected-react-router';
 import { Switch, Route } from 'react-router-dom';
 import loadable from '@loadable/component';
-import { Layout, Alert } from 'antd';
-import { selectError, deactivate } from '~/features/web3/web3Slice';
-import getErrorMessage from '~/features/web3/getErrorMessage';
+import { Layout } from 'antd';
 import { Spin } from '~/adapters/antd';
-import WalletConnectionButton from '~/components/WalletConnectionButton';
-import Button from '~/components/Button';
+import Web3ErrorAlert from '~/components/Web3ErrorAlert';
 import Navbar from '~/components/Navbar';
 import Footer from '~/components/Footer';
 import { DrawerMenu } from '~/components/Menu';
@@ -32,7 +28,7 @@ function MainRouter() {
         <DrawerMenu />
         <Layout>
           <Navbar />
-          <Web3ErrorBanner />
+          <Web3ErrorAlert />
           <StyledContent>
             <Switch>
               <Route exact path={r.HOME}>
@@ -63,37 +59,6 @@ function MainRouter() {
 }
 
 export default MainRouter;
-
-function Web3ErrorBanner() {
-  const dispatch = useDispatch();
-  const web3Error = useSelector(selectError);
-
-  const handleDisconnectClick = React.useCallback(
-    evt => {
-      evt.preventDefault();
-      dispatch(deactivate());
-    },
-    [dispatch]
-  );
-
-  return web3Error ? (
-    <Alert
-      banner
-      showIcon={false}
-      type="warning"
-      message={<>{getErrorMessage(web3Error)}</>}
-      description={
-        <>
-          You could{' '}
-          <Button variant="link" onClick={handleDisconnectClick}>
-            use Linguo in read-only mode
-          </Button>{' '}
-          or try to <WalletConnectionButton variant="link">connect to a different wallet</WalletConnectionButton>.
-        </>
-      }
-    />
-  ) : null;
-}
 
 const StyledContent = styled(Layout.Content)`
   height: 100%;
