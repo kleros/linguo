63852ce60827ece20f8c00df32163c81ec7a27f3
gratestas <stasgrate@gmail.com>
2023-01-09 15:41:21 +0300

    refactor: refactor connection button and change its location

---
+++ src/components/ConnectionButton.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useWeb3 } from '../hooks/useWeb3';
+import { useConnect } from '../hooks/useConnect';
+import { SUPPORTED_CHAINS } from '../consts/supportedChains';
+import Button from '~/shared/Button';
+
+const shortenAddress = address => `${address.slice(0, 6)}...${address.slice(-4)}`;
+
+const ConnectButton = () => {
+  const { active } = useWeb3();
+  const { activate, connecting } = useConnect();
+  return active ? <AccountDisplay /> : <Button disabled={connecting} small text={'Connect'} onClick={activate} />;
+};
+
+export default ConnectButton;
+
+const AccountDisplay = () => {
+  const { account, chainId } = useWeb3();
+  const chainName = chainId ? SUPPORTED_CHAINS[chainId].chainName : undefined;
+  const shortAddress = account ? shortenAddress(account) : undefined;
+  return (
+    <StyledContainer>
+      <small>{chainName}</small>
+      <label>{shortAddress}</label>
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled.div`
+  width: fit-content;
+  height: 34px;
+  padding: 16px;
+  gap: 0.5rem;
+  border-radius: 300px;
+  background-color: ${({ theme }) => theme.whiteLowOpacity};
+  display: flex;
+  align-items: center;
+  > label {
+    color: ${({ theme }) => theme.primaryText};
+  }
+  :before {
+    content: '';
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.success};
+  }
+`;
