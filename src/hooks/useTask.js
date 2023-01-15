92bee8083ebc55b2bae6f89254630f78c2659c8a
gratestas <stasgrate@gmail.com>
2023-01-15 13:56:02 +0300

    refactor: refactor 'useTask' hook

--- src/hooks/useTask.js
+++ src/hooks/useTask.js
@@ -1,74 +1,59 @@
-import React from 'react';
-
 import { useTaskQuery } from './queries/useTaskQuery';
-import { useIPFSQuery } from './queries/useIPFSQuery';
-
-import useInterval from '~/shared/useInterval';
+import { useMetaEvidenceQuery } from './queries/useMetaEvidenceQuery';
 
 import { useWeb3 } from './useWeb3';
+import { useCurrentTaskPrice } from './useCurrentTaskPrice';
 
 import Task from '~/utils/task';
-import taskStatus from '~/consts/taskStatus';
-import { _1_MINUTE_MS } from '~/consts/time';
 
 export const useTask = id => {
   const { account } = useWeb3();
   const { task, isLoading, isError } = useTaskQuery(id);
-  const { data } = useIPFSQuery(task?.metaEvidence?.URI);
+  const { metadata, isLoading: isMetadataLoading } = useMetaEvidenceQuery(task?.metaEvidence?.URI);
 
   const {
+    assignedPrice,
     challenger,
     lastInteraction,
     numberOfRounds,
     requester,
-    requesterDeposit,
     translation,
     translator,
     status,
     submissionTimeout,
   } = task;
 
-  const { minPrice, maxPrice, wordCount } = data?.metadata;
+  const { minPrice, maxPrice, wordCount } = metadata;
 
   const latestRoundId = `${id}-${Number(numberOfRounds) - 1}`;
-  const [currentPrice, setCurrentPrice] = React.useState(
-    Task.getCurrentPrice(requesterDeposit, minPrice, maxPrice, lastInteraction, submissionTimeout, status)
+  const isPending = Task.isPending(status);
+  const isIncomplete = Task.isIncomplete(status, translation, lastInteraction, submissionTimeout);
+
+  const { currentPrice, pricePerWord } = useCurrentTaskPrice(
+    status,
+    minPrice,
+    maxPrice,
+    assignedPrice,
+    wordCount,
+    lastInteraction,
+    submissionTimeout
   );
 
-  const updateCurrentPrice = React.useCallback(() => {
-    const value = Task.getCurrentPrice(
-      requesterDeposit,
-      minPrice,
-      maxPrice,
-      lastInteraction,
-      submissionTimeout,
-      status
-    );
-    setCurrentPrice(value);
-  }, [lastInteraction, maxPrice, minPrice, requesterDeposit, status, submissionTimeout]);
-
-  const interval = requesterDeposit === undefined ? _1_MINUTE_MS : null;
-  useInterval(updateCurrentPrice, interval);
-
-  const actualPrice = status === taskStatus.Assigned ? requesterDeposit : currentPrice;
-  const pricePerWord = Task.getCurrentPricePerWord(actualPrice, wordCount);
-
-  const isIncomoplete = Task.isIncomplete(status, translation, lastInteraction, submissionTimeout);
   const currentParty = Task.getCurrentParty(account, requester, translator, challenger);
 
   return {
     task: {
       ...task,
-      ...data?.metadata,
-      actualPrice,
+      ...metadata,
       currentPrice,
       pricePerWord,
       currentParty,
       latestRoundId,
       isLoading,
-      isIncomoplete,
+      isIncomplete,
+      isPending,
     },
-    isLoading,
+    isLoading: isLoading || isMetadataLoading,
     isError,
   };
 };
