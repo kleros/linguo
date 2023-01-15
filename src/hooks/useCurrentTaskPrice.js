7e89384e981c4968d581da2402d9e2b16401a80c
gratestas <stasgrate@gmail.com>
2023-01-15 13:54:37 +0300

    refactor: create hook to calculate task's current price

---
+++ src/hooks/useCurrentTaskPrice.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import taskStatus from '~/consts/taskStatus';
+import useInterval from '~/shared/useInterval';
+import Task from '~/utils/task';
+
+import { _1_MINUTE_MS } from '~/consts/time';
+
+export const useCurrentTaskPrice = (
+  status,
+  minPrice,
+  maxPrice,
+  assignedPrice,
+  wordCount,
+  lastInteraction,
+  submissionTimeout
+) => {
+  const [currentPrice, setCurrentPrice] = React.useState(
+    Task.getCurrentPrice(assignedPrice, minPrice, maxPrice, lastInteraction, submissionTimeout)
+  );
+
+  const updateCurrentValue = React.useCallback(() => {
+    const value = Task.getCurrentPrice(assignedPrice, minPrice, maxPrice, lastInteraction, submissionTimeout);
+    setCurrentPrice(value);
+  }, [assignedPrice, minPrice, maxPrice, lastInteraction, submissionTimeout]);
+
+  const interval = status === taskStatus.Created ? _1_MINUTE_MS : null;
+  useInterval(updateCurrentValue, interval);
+
+  const pricePerWord = Task.getCurrentPricePerWord(currentPrice, wordCount);
+
+  return { currentPrice, pricePerWord };
+};
